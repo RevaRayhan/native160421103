32d44a160ec1411fc60643959829eb7f4ceed09f
RevaRayhan <revarayhansyah@gmail.com>
2024-02-26 11:36:19 +0700

    W2 no animation

--- app/src/main/java/com/example/anmp_w1/MainFragment.kt
+++ app/src/main/java/com/example/anmp_w1/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.anmp_w1.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -12,11 +14,21 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class MainFragment : Fragment() {
+    private lateinit var binding:FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnStart.setOnClickListener {
+            val playerName = binding.txtName.text.toString()
+            val action = MainFragmentDirections.actionMainFragmentToGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
